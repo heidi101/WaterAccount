d6112e5dcb6951f961c96cfd32a2274f2d954006
heidi101 <heidi.lou@students.tka.org>
2022-05-21 22:51:34 -0700

    Initial Commit

---
+++ WaterAccount/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WaterAccount
+//
+//  Created by HL on 5/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaterAccount/WaterAccountApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterAccountApp.swift
+//  WaterAccount
+//
+//  Created by HL on 5/21/22.
+//
+
+import SwiftUI
+
+@main
+struct WaterAccountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
